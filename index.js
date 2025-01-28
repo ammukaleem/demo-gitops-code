344e25e1a188cabfa149603b18309d39dfc2d572
ammukaleem <145837922+ammukaleem@users.noreply.github.com>
2025-01-28 21:22:50 +0530

    update final

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
+<<<<<<< HEAD
+  res.send('Hi kaleem welcome to GitOps.!!!')
+=======
   res.send('Congrats HAFSA CICD Success!!!')
+>>>>>>> df40dd62b139f96883782cdafaa7c828e1dfce7f
 })
 
 app.listen(port, () => {
