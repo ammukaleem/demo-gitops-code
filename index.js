df40dd62b139f96883782cdafaa7c828e1dfce7f
ammukaleem <mdkaleem77@gmail.com>
2025-01-28 12:29:58 +0000

    index changed

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Congrats Kaleem CICD Success!!!')
+  res.send('Congrats HAFSA CICD Success!!!')
 })
 
 app.listen(port, () => {
