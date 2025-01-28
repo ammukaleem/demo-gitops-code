559f403ee0c9cfddfea7f42e8561c78ee5572c82
ammukaleem <145837922+ammukaleem@users.noreply.github.com>
2025-01-28 21:38:27 +0530

    juveria

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-res.send('Hi Ammmaar welcome to GitOps.!!!')
+res.send('Hi Juveria welcome to GitOps.!!!')
 res.send('Congrats HAFSA CICD Success!!!')
 })
 
