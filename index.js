5ec78a02f1bf49486b0130ffabbc77de4669d32d
ammukaleem <mdkaleem77@gmail.com>
2025-01-29 15:40:43 +0000

    kaleem

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
+<<<<<<< HEAD
 res.send('Hi Sowdager  welcome to GitOps Argocd!!!')
+=======
+  res.send('Congrats kaleem CICD Success!!!')
+>>>>>>> de0f2cf (kaleem)
 })
 
 app.listen(port, () => {
